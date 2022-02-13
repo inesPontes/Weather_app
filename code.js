27286280938fbc1144655d953cf80109c7509e43
Ines <inespontes110@gmail.com>
2022-02-13 15:27:03 +0000

    Improve default city weather information

--- code.js
+++ code.js
@@ -1,24 +1,52 @@
-//Substitute Today´s date
+//Default city weather information
 
-let newDate = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+  ];
 
-let day = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thrusday",
-  "Friday",
-  "Saturday",
-];
+  let weekday = day[date.getDay()];
+  return `${weekday} ${hours}:${minutes}`;
+}
 
-let weekday = day[newDate.getDay()];
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  axios.get(apiUrl).then(showWeather);
+  let temperatureElement = document.querySelector(".temp");
+  temperatureElement.innerHTML = `${temperature} ºC`;
+  let description = response.data.weather[0].main;
+  let descriptionElement = document.querySelector(".weather-summary");
+  descriptionElement.innerHTML = `${description}`;
+  let windValue = response.data.wind.speed;
+  let windElement = document.querySelector(".wind");
+  windElement.innerHTML = `Wind Speed: ${windValue}km/h`;
+  let maxTemp = Math.round(response.data.main.temp_max);
+  let minTemp = Math.round(response.data.main.temp_min);
+  let tempIntervaleElement = document.querySelector(".temp_intervale");
+  tempIntervaleElement.innerHTML = `${minTemp} - ${maxTemp}ºC`;
+  let dateElement = document.querySelector(".todaysDate");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
 
-let hour = newDate.getHours();
-let minute = newDate.getMinutes();
-
-let today = document.querySelector(".todaysDate");
-today.innerHTML = `${weekday} ${hour}:${minute}`;
+let defaultCity = "Lisbon";
+let apiKey = "e5b84de8f6e41d7411771c16118293ab";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&units=metric&appid=${apiKey}`;
+axios.get(apiUrl).then(displayTemperature);
 
 //Search Engine
 
@@ -29,6 +57,7 @@ function changeCity() {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location.value}&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(showWeather);
 }
+
 function showWeather(response) {
   document.querySelector(".exampleCity").innerHTML = response.data.name;
   let temperature = Math.round(response.data.main.temp);
@@ -49,7 +78,7 @@ function showWeather(response) {
 let form = document.querySelector(".cityname");
 form.addEventListener("submit", changeCity);
 
-//week 5 - Current Location
+//Current Location
 
 function searchLocation(position) {
   let lat = position.coords.latitude;
