324b5d3303351617e4555dceeba43fc9d1651cb0
Ines <inespontes110@gmail.com>
2022-02-13 21:10:16 +0000

    optimize functions

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-//Default city weather information
+//Format Date
 
 function formatDate(timestamp) {
   let date = new Date(timestamp);
@@ -25,14 +25,15 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+  let exampleCity = document.querySelector(".exampleCity");
+  exampleCity.innerHTML = response.data.name;
   let temperature = Math.round(response.data.main.temp);
-  axios.get(apiUrl).then(showWeather);
   let temperatureElement = document.querySelector(".temp");
   temperatureElement.innerHTML = `${temperature} ºC`;
   let description = response.data.weather[0].main;
   let descriptionElement = document.querySelector(".weather-summary");
   descriptionElement.innerHTML = `${description}`;
-  let windValue = Math.round(response.data.wind.speed);
+  let windValue = response.data.wind.speed;
   let windElement = document.querySelector(".wind");
   windElement.innerHTML = `Wind Speed: ${windValue}km/h`;
   let maxTemp = Math.round(response.data.main.temp_max);
@@ -41,62 +42,48 @@ function displayTemperature(response) {
   tempIntervaleElement.innerHTML = `${minTemp} - ${maxTemp}ºC`;
   let dateElement = document.querySelector(".todaysDate");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  celsiusTemperature = response.data.main.temp;
+
   let iconElement = document.querySelector("#icon");
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/1${response.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
-let defaultCity = "Lisbon";
-let apiKey = "e5b84de8f6e41d7411771c16118293ab";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&units=metric&appid=${apiKey}`;
-axios.get(apiUrl).then(displayTemperature);
-
-//Search Engine
-
-function changeCity() {
-  event.preventDefault();
-  let location = document.querySelector("#inputPassword5");
+function searchCity(location) {
   let apiKey = "e5b84de8f6e41d7411771c16118293ab";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location.value}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(showWeather);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function showWeather(response) {
-  document.querySelector(".exampleCity").innerHTML = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
-  let temperatureElement = document.querySelector(".temp");
-  temperatureElement.innerHTML = `${temperature} ºC`;
-  let description = response.data.weather[0].main;
-  let descriptionElement = document.querySelector(".weather-summary");
-  descriptionElement.innerHTML = `${description}`;
-  let windValue = Math.round(response.data.wind.speed);
-  let windElement = document.querySelector(".wind");
-  windElement.innerHTML = `Wind Speed: ${windValue}km/h`;
-  let maxTemp = Math.round(response.data.main.temp_max);
-  let minTemp = Math.round(response.data.main.temp_min);
-  let tempIntervaleElement = document.querySelector(".temp_intervale");
-  tempIntervaleElement.innerHTML = `${minTemp} - ${maxTemp}ºC`;
+function handleSubmit() {
+  event.preventDefault();
+  let location = document.querySelector("#inputPassword5");
+  searchCity(location.value);
 }
 
 let form = document.querySelector(".cityname");
-form.addEventListener("submit", changeCity);
+form.addEventListener("submit", handleSubmit);
 
 //Current Location
+
+function showPosition(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+  getCordinates(coordinates);
+}
+
+let currentButton = document.querySelector(".currentLocation");
+currentButton.addEventListener("click", showPosition);
 
 function searchLocation(position) {
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
   let apiKey = "e5b84de8f6e41d7411771c16118293ab";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(showWeather);
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function showPosition(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let currentButton = document.querySelector(".currentLocation");
-currentButton.addEventListener("click", showPosition);
+searchCity("Lisbon");
